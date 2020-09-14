57021096fd3a9f022f3d542dfc7b8bdf5632d9cb
SOPHIE JIANG <sj.sophiejiang@gmail.com>
2020-09-13 23:20:19 -0600

    mighty heave ho

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home">
+      <div className="title">
+        hey there! 
+      </div>
+    </div>
+  );
+}
+
+export default Home;
