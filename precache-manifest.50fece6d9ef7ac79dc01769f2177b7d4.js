2908e0558e5c5866151e6efa864bfc4899c0e382
Sophie Jiang <sj.sophiejiang@gmail.com>
2021-11-23 13:37:32 -0800

    Updates

---
+++ precache-manifest.50fece6d9ef7ac79dc01769f2177b7d4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc9c0c8992b543eb5b119796737f0af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0a2cbfa6b766033f53c",
+    "url": "/static/css/2.862b2f9f.chunk.css"
+  },
+  {
+    "revision": "ed94fd4e4d558a1f12cc",
+    "url": "/static/css/main.3bfb68b7.chunk.css"
+  },
+  {
+    "revision": "e0a2cbfa6b766033f53c",
+    "url": "/static/js/2.2b7480a4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.2b7480a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed94fd4e4d558a1f12cc",
+    "url": "/static/js/main.34598461.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "fd2e489c37b7a37ed3c8da0cf8d68716",
+    "url": "/static/media/about_me.fd2e489c.png"
+  },
+  {
+    "revision": "32b827efe24be9c10a286a2522b65ada",
+    "url": "/static/media/home_profile.32b827ef.png"
+  },
+  {
+    "revision": "67a009bb8d297bc139c8879782589333",
+    "url": "/static/media/ix.67a009bb.jpg"
+  },
+  {
+    "revision": "2ad4f919eee661796eb62b3b4e026082",
+    "url": "/static/media/logo.2ad4f919.png"
+  },
+  {
+    "revision": "77bf6741bb83cd8adfd5921eb59bf5fa",
+    "url": "/static/media/logo_hover.77bf6741.png"
+  },
+  {
+    "revision": "7235f2eae793601b46c423a6b0bc0453",
+    "url": "/static/media/osc.7235f2ea.png"
+  },
+  {
+    "revision": "30bee51e6c03eff2c809527e2c11537f",
+    "url": "/static/media/resume.30bee51e.pdf"
+  },
+  {
+    "revision": "74a77e03b1ed825c1e4bbae440095e17",
+    "url": "/static/media/splunk.74a77e03.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8490899bd939656282a90a4c9908f038.js"
+  "/precache-manifest.50fece6d9ef7ac79dc01769f2177b7d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
